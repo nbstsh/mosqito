6da3f99aa809d03609182ab32b5fb21213cb0a86
nbstsh <nbstsh@gmail.com>
2019-08-03 18:33:00 +0900

    Refactoring hand detection utils

--- src/components/HandDetection/HandDetection.utils.js
+++ src/components/HandDetection/HandDetection.utils.js
@@ -1,20 +1,6 @@
 import { useState, useRef, useEffect } from 'react';
 import * as handTrack from 'handtrackjs';
 
-let model, videoEl, canvasEl;
-
-const setModel = val => {
-	model = val;
-};
-
-const setVideoEl = el => {
-	videoEl = el;
-};
-
-const setCanvasEl = el => {
-	canvasEl = el;
-};
-
 const modelParams = {
 	flipHorizontal: true, // flip e.g for video
 	imageScaleFactor: 0.5, // reduce input image size for gains in speed.
@@ -23,7 +9,7 @@ const modelParams = {
 	scoreThreshold: 0.79 // confidence threshold for predictions.
 };
 
-const startVideo = async () => {
+const startVideo = async videoEl => {
 	if (!videoEl) throw new Error('Video element needs to be set.');
 
 	const status = await handTrack.startVideo(videoEl);
@@ -42,81 +28,41 @@ const loadModel = async () => {
 	return await handTrack.load(modelParams);
 };
 
-const initHandDetection = async () => {
+const initHandDetection = async videoEl => {
 	try {
-		await startVideo();
-		const model = await loadModel();
-		setModel(model);
+		await startVideo(videoEl);
+		return await loadModel();
 	} catch (e) {
 		handTrack.stopVideo(videoEl);
 		console.error(e);
 	}
 };
 
-export const runDetection = async handler => {
-	if (!model) throw new Error('Model needs to to be set.');
-	if (!videoEl) throw new Error('video element needs to be set.');
-
-	const predictions = await model.detect(videoEl);
-	handler && handler(predictions);
-
-	// in case you want to render prediction, comment out lines beloew and setCanvas() in useHandDetection;
-
-	// if (!canvasEl) throw new Error('Canvas element needs to be set.');
-	// model.renderPredictions(
-	// 	predictions,
-	// 	canvasEl,
-	// 	canvasEl.getContext('2d'),
-	// 	videoEl
-	// );
-};
-
-export const useHandDetection = () => {
-	const videoRef = useRef(null);
-	const canvasRef = useRef(null);
-	const [isReady, setIsReady] = useState(false);
-
-	useEffect(() => {
-		setVideoEl(videoRef.current);
-		setCanvasEl(canvasRef.current);
-		initHandDetection()
-			.then(() => setIsReady(true))
-			.catch(err => console.error('Something went wrong at init.'));
-	}, []);
-
-	return {
-		videoRef,
-		canvasRef,
-		isReady
-	};
-};
-
 export const useHandDetectionWithPredictinos = (interval = 500) => {
 	const videoRef = useRef(null);
-	const canvasRef = useRef(null);
+	const [model, setModel] = useState(null);
 	const [isReady, setIsReady] = useState(false);
 	const [predictions, setPredictions] = useState(null);
 
 	useEffect(() => {
-		setVideoEl(videoRef.current);
-		setCanvasEl(canvasRef.current);
 		initHandDetection(videoRef.current)
-			.then(() => setIsReady(true))
+			.then(setModel)
 			.catch(err => console.error('Something went wrong at init.'));
 	}, []);
 
 	useEffect(() => {
-		if (!isReady) return;
+		if (!model) return;
 
-		const proceedDetection = () => {
-			runDetection(predictions => setPredictions(predictions));
+		const proceedDetection = async () => {
+			const predictions = await model.detect(videoRef.current);
+			setPredictions(predictions);
 		};
 		setInterval(proceedDetection, interval);
-	}, [isReady, interval]);
+		setIsReady(true);
+	}, [model, interval]);
 
 	return {
 		videoRef,
-		canvasRef,
 		isReady,
 		predictions
 	};
